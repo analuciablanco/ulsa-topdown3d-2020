22e250f3aaa71a184cbb2a76685b2ecf08028027
Ana Lucia Blanco <analuciablanco@outlook.com>
2020-03-26 22:41:24 -0700

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed;
+
+    void Update()
+    {
+        transform.Translate(Axis.normalized.magnitude * Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if(Axis != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(Axis.normalized);
+        }
+    }
+
+    Vector3 Axis
+    {
+        get => new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+}
