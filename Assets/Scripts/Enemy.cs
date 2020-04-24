c799a6aa5a1e2787ad3857d2fdc3476b15ab912d
Ana Lucia Blanco <analuciablanco@outlook.com>
2020-04-24 01:02:46 -0700

    enemy follows player and avoids obstacles

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField, Range(0.1f, 10f)] float moveSpeed;
     [SerializeField, Range(0f, 10f)] float minDistance;
 
+    NavMeshAgent navMeshAgent;
+
+    void Awake() {
+        navMeshAgent = GetComponent<NavMeshAgent>();    
+    }
+
     void Update() {
         if(Attack)
         {
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            //transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            navMeshAgent.destination = GameManager.instance.Player.transform.position;
             transform.LookAt(GameManager.instance.Player.transform);
         }
     }
